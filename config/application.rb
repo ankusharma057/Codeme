bbc434c555e4a6b494c03d9d6e50cf1fa30a4dd6
Ashrut Panchal <ashrutpanchal111@gmail.com>
2024-09-26 16:03:41 +0530

    new

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module NotFoodDemo
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.assets.enabled = true
 
     # Configuration for the application, engines, and railties goes here.
     #
